91fff93d85cdfca57e036e7ee800483c640bd908
cahalinc <cahalinc488@gmail.com>
2026-05-21 15:35:17 +0900

    Add files via upload

--- contact.php
+++ contact.php
@@ -41,7 +41,7 @@ $comments = htmlspecialchars($comments);
 // Enter the email address that you want to emails to be sent to.
 
 //$address = "example@youremail.net";
-$address = "clapatform@gmail.com";
+$address = "keiohigh2nd@gmail.com";
 
 
 // Configuration option.
